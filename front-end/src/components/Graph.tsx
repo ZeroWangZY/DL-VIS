852e2f2c555cede8ae6b3bade18adc7a06a41744
ZeroWangZY <756762961@qq.com>
2020-01-15 20:06:07 +0800

    feat: add graph and graph scene component

--- front-end/src/components/Graph.tsx
+++ front-end/src/components/Graph.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import { getDemoGraphAPI } from "../api/graph";
+import "./Graph.css";
+import GraphScene from "./GraphScene";
 
 const Graph: React.FC = () => {
   getDemoGraphAPI();
-  return <div className="Graph">graph here</div>;
+  const title = "main graph";
+  return (
+    <div className="container">
+      <div className="vertical">
+        <h2>{title}</h2>
+        {/* <graph-scene
+          id="scene"
+          className="auto"
+          render-hierarchy="[[renderHierarchy]]"
+          highlighted-node="[[_getVisible(highlightedNode)]]"
+          selected-node="{{selectedNode}}"
+          color-by="[[colorBy]]"
+          progress="[[progress]]"
+          node-names-to-health-pills="[[nodeNamesToHealthPills]]"
+          health-pill-step-index="{{healthPillStepIndex}}"
+        ></graph-scene> */}
+        <GraphScene></GraphScene>
+      </div>
+    </div>
+  );
 };
 
 export default Graph;
