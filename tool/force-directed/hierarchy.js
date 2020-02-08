92e1f537f9965605d27fb467fc30aa5464f833dc
xiaoaibi <370064808@qq.com>
2020-02-08 17:04:46 +0800

    hierarchy

---
+++ tool/force-directed/hierarchy.js
@@ -0,0 +1,159 @@
+//聚合为层级数据
+const NAMESPACE_DELIM = '/';
+// class Hierarchy {
+    
+// }
+function getStrictName(name){
+    let parts = name.split(NAMESPACE_DELIM);
+    return name + NAMESPACE_DELIM + '(' + parts[parts.length - 1] + ')';
+}
+
+function startsWith(string, target, position) {
+    const { length } = string
+    position = position == null ? 0 : position
+    if (position < 0) {
+      position = 0
+    }
+    else if (position > length) {
+      position = length
+    }
+    target = `${target}`
+    return string.slice(position, position + target.length) == target
+}
+
+// function bulidNodeOpMap(graph){
+//   let nodes = graph.nodes
+
+// }
+
+function getHierarchicalPath(name){
+    let path = [];
+    let i = name.indexOf(NAMESPACE_DELIM);
+    while (i >= 0) {
+      path.push(name.substring(0, i));
+      i = name.indexOf(NAMESPACE_DELIM, i + 1);
+    }
+    path.push(name);
+    return path;
+  };
+function mapStrictHierarchy(nodeNames) {
+    /** Dictionary that maps the old new to the new name */
+    let newNameDictionary = {};
+    let namespaceSet = {};
+    nodeNames.sort();
+    for (let i = 0; i < nodeNames.length - 1; ++i) {
+      let a = nodeNames[i];
+      getHierarchicalPath(a).slice(0, -1).map(ns => {
+        namespaceSet[ns] = true;
+      });
+      for (let j = i + 1; j < nodeNames.length; ++j) {
+        let b = nodeNames[j];
+        if (startsWith(b, a)) {
+          if (b.length > a.length && b.charAt(a.length) === NAMESPACE_DELIM) {
+            newNameDictionary[a] = getStrictName(a);
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+    return newNameDictionary;
+};
+function hierarchyNode(graph){
+    let nodes = graph.nodes
+    graph.depth = 0
+    let nodeMap = {}
+    // let nodeNames = nodes.map(d=>d.id)
+    // let nodeNamesDict = mapStrictHierarchy(nodeNames)
+    
+    for(let node of nodes){
+  //      node.id = nodeNamesDict[node.id] || node.id;
+        let path = node.id.split('/')
+        graph.depth = Math.max(path.length, graph.depth)
+        let parent = nodeMap
+        for (let i = 0; i < path.length; i++) {
+            if(!parent.hasOwnProperty(path[i])) {
+                parent[path[i]] = {}
+            }
+            parent = parent[path[i]]
+        }
+    }
+    graph.hierarchyNode = nodeMap
+    return graph
+}
+function hierarchyEdge(graph){
+    let links = graph.links
+    let hierarchyNode = graph.hierarchyNode
+    let hierarchy = {}
+    let test = []
+    if(JSON.stringify(hierarchyNode) !== '{}'){
+      let path = ''
+      hierarchy = classfyEdge(hierarchyNode,links,path)
+    }
+    //由map形式整理为数组
+
+    
+
+    graph.hierarchy = hierarchy
+}
+function classfyEdge(nodes,links,path){
+  let hierarchyEdge = {}
+  for(let node in nodes){
+    if(JSON.stringify(nodes[node]) !== '{}'){
+      nodes[node] = classfyEdge(nodes[node],links,`${path}${node}/`)
+    }
+  }
+
+  for(let link of links){
+   //   let indexS = link.source.startsWith(path);  
+     
+    //  link.source  = link.source.substring(0, indexS+1);
+
+ //     let indexT = link.target.startsWith(path);  
+
+      let source = link.source.replace(path, "").split('/')[0];
+      let target = link.target.replace(path, "").split('/')[0];
+  //    link.target  = link.target.substring(0, indexT+1);
+
+      if(link.source.startsWith(path)&&link.target.startsWith(path)&&nodes.hasOwnProperty(source)&&nodes.hasOwnProperty(target)&&source!==target){
+        hierarchyEdge[source] = target
+      }
+  }
+  return {
+    nodes,
+    links: hierarchyEdge
+  }
+}
+function hierarchyBuild(graph){
+  //层级名相同的修改name
+  let nodes = graph.nodes
+  let nodeNames = nodes.map(d=>d.id)
+  let nodeNamesDict = mapStrictHierarchy(nodeNames)
+  let nodeOpMap = {}
+ // 
+  let links = graph.links
+  for(let node of nodes){
+    node.id = nodeNamesDict[node.id] || node.id;
+    nodeOpMap[node.id] = node.group
+  }
+  graph.nodeOpMap = nodeOpMap
+  for(let link of links){
+    link.source = nodeNamesDict[link.source] || link.source;
+    link.target = nodeNamesDict[link.target] || link.target;
+  }
+ // bulidNodeOpMap(graph)
+  hierarchyNode(graph)
+ // console.log(graph)
+  hierarchyEdge(graph)
+  //return nodes
+  console.log(graph)
+}
+
+
+
+fetchAndParseGraphData('./pbtxtdata/test-graph-1.pbtxt').then(pbtxtData => {
+    graph = buildGraph(pbtxtData);
+    let test = hierarchyBuild(graph)
+  // console.log(test)
+})
