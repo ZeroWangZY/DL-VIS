7d67afef7700f38fc4cf3a913d78dca883037b73
Jingli Xu <1220341948@qq.com>
2020-07-31 09:59:05 +0800

    fix: 最后一个layernode折线图不显示的问题

--- front-end/src/components/LayerLevel/FindChildNodeUnderLayerNode.tsx
+++ front-end/src/components/LayerLevel/FindChildNodeUnderLayerNode.tsx
@@ -19,13 +19,18 @@ export const FindChildNodeUnderLayerNode = (nodeMap, NodeId: string) => {
 
   function BFS(root) {
     if (root === null) return;
+    let operationNodeOutGegreeMap = new Map();
+
     let res = [], queue = [];
     queue.push(root);
     while (queue.length !== 0) {
       let node = queue.shift();
       if (node instanceof OperationNodeImp) {
+        if (!operationNodeOutGegreeMap.has(node.id))
+          operationNodeOutGegreeMap.set(node.id, 0);
         (node as OperationNodeImp).outputNode.forEach((outputNodeId) => {
           // outputNodeId 一定是一个 operation Node
+          operationNodeOutGegreeMap.set(node.id, operationNodeOutGegreeMap.get(node.id) + 1);
           let scopeOfOutputNode = (nodeMap[outputNodeId] as OperationNodeImp).parent;
           // 判断scope2是否在scope1同级或者下面
           // 也就是判断scope2是否以scope1开头
@@ -36,6 +41,12 @@ export const FindChildNodeUnderLayerNode = (nodeMap, NodeId: string) => {
         (node as GroupNodeImp).children.forEach((nodeId) => {
           queue.push(nodeMap[nodeId]);
         })
+      }
+    }
+    if (res.length === 0) {
+      for (var [operationNodeId, degree] of operationNodeOutGegreeMap) {
+        if (degree === 0)
+          res.push(operationNodeId);
       }
     }
     return res;
