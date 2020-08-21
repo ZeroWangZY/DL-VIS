f8b2010eabee7485af13b03f82b5bcbac163321d
ZeroWangZY <756762961@qq.com>
2020-08-21 12:05:14 +0800

    feat: 重写裁剪backbone以外节点的逻辑

--- front-end/src/common/graph-processing/stage1/raw-graph-optimizer.ms.ts
+++ front-end/src/common/graph-processing/stage1/raw-graph-optimizer.ms.ts
@@ -1,86 +1,9 @@
 import { RawNode, RawGraph } from "./raw-graph.ms.type";
-import { RawGraphOptimizer } from "./raw-graph-optimizer.tf";
 
-const conceptualGraphOptimization = (newRawGraph: RawGraph): void => {
-  // mobilenetv2 只保留输出为 RawNode.name === "211"节点 的子图
-  // alexnets 只保留输出为RawNode.name === "23"节点 的子图
-  console.log("我要看的位置：")
-  console.log(newRawGraph.node)
-  let nodes = newRawGraph.node
-  let newNodes: RawNode[] = [];
-
-  // 查询scope中有backbone的位置
-  for (let idx = 0, len = nodes.length; idx < len; idx++) {
-    if (nodes[idx].scope.indexOf("backbone") !== -1
-    && nodes[idx].scope.indexOf("network") !== -1
-    && nodes[idx].scope.indexOf("Default") !== -1) {
-      newNodes.push(nodes[idx]);
-    }
-  }
-
-  newRawGraph.node = newNodes;
-  console.log("修改后的整个图")
-  console.log(newRawGraph)
-
-  // if (newRawGraph.name === "455_454_403_352_construct") {
-  //   // alexnets
-  //   pruneByOutput(newRawGraph, "23");
-  // }
-  // else if (newRawGraph.name === "7217_7215_7213_5169_2585_1_construct")
-  //   // mobilenetv2
-  //   pruneByOutput(newRawGraph, "211");
-
-};
-
-const pruneByOutput = (newRawGraph: RawGraph, outputNodeName: string) => {
-  let NameIndexMap: Map<string, number> = new Map(); // key:节点name  value: 在RawGraph.node数组中的下标
-  let nodes = newRawGraph.node;
-  for (let i = 0, len = nodes.length; i < len; i++) {
-    NameIndexMap.set(nodes[i].name, i);
-  }
-
-  let indexOfNodesToBeKept = [];
-  findIndexOfNodesToBeKept(
-    nodes,
-    indexOfNodesToBeKept,
-    NameIndexMap,
-    outputNodeName
-  );
-
-  let newNodes: RawNode[] = [];
-  for (let idx of indexOfNodesToBeKept) {
-    newNodes.push(nodes[idx]);
-  }
-  newRawGraph.node = newNodes;
-};
-
-const findIndexOfNodesToBeKept = (
-  nodes: RawNode[],
-  indexOfNodesToBeKept: number[],
-  NameIndexMap: Map<string, number>,
-  outputNodeName: string
-) => {
-  // nodes[NameIndexMap]
-  let nodeIndex = NameIndexMap.get(outputNodeName);
-  indexOfNodesToBeKept.push(nodeIndex);
-
-  let node = nodes[nodeIndex];
-  let inputs = node.input;
-  if (inputs === undefined || inputs === null) return;
-
-  for (let input of inputs) {
-    let inputNodeName = input.name;
-    if (NameIndexMap.has(inputNodeName)) {
-      findIndexOfNodesToBeKept(
-        nodes,
-        indexOfNodesToBeKept,
-        NameIndexMap,
-        inputNodeName
-      );
-      NameIndexMap.delete(inputNodeName); // 防止重复
-    }
-  }
-};
+const BACKBONE_PATTERNS: Set<string[]> = new Set([
+  ['Default', 'network', 'backbone'],
+  ['Default', 'network', 'bert']
+])
 
 // 发现ms图中有大量tuple_getitem算子，且意义不大，此方法用于去除这些算子
 function pruneTupleGetItem(rawGraph: RawGraph): void {
@@ -126,13 +49,75 @@ function _buildTargetsMap(rawGraph: RawGraph): { [id: string]: any } {
   return resDict;
 }
 
+function keepOnlyBackbone(rawGarph: RawGraph) {
+  const regexPattern = _genBackboneRegex()
+
+  // 仅保留backbone里的节点
+  const reservedNodes: RawNode[] = []
+  for (const n of rawGarph.node) {
+    if (n.scope.search(regexPattern) !== -1) {
+      reservedNodes.push(n)
+    }
+  }
+  rawGarph.node = reservedNodes
+
+  // 清理const 和 parameter
+  const existConstsAndParameters = new Set()
+  const reservedConsts = []
+  const reservedParameters = []
+  for (const node of rawGarph.node) {
+    for (const input of node.input) {
+      existConstsAndParameters.add(input)
+    }
+  }
+  for (const cst of rawGarph.constVals) {
+    if (existConstsAndParameters.has(cst.key)) {
+      reservedConsts.push(cst)
+    }
+  }
+  for (const para of rawGarph.parameters) {
+    if (existConstsAndParameters.has(para.name)) {
+      reservedParameters.push(para)
+    }
+  }
+  rawGarph.constVals = reservedConsts
+  rawGarph.parameters = reservedParameters
+
+  // 清理node里的input
+  const reservedInput: Set<string> = new Set()
+  rawGarph.node.forEach(node => reservedInput.add(node.name))
+  rawGarph.constVals.forEach(cst => reservedInput.add(cst.key))
+  rawGarph.parameters.forEach(para => reservedInput.add(para.name))
+  for (const node of rawGarph.node) {
+    const newInput = []
+    for (const input of node.input) {
+      if (reservedInput.has(input.name)) {
+        newInput.push(input)
+      }
+    }
+    node.input = newInput
+  }
+}
+
+function _genBackboneRegex(): string {
+  let regexPattern = ''
+  for (const pattern of BACKBONE_PATTERNS) {
+    regexPattern += '(^'
+    for (const p of pattern) {
+      regexPattern += p + ".*"
+    }
+    regexPattern += ')|'
+  }
+  return regexPattern.slice(0, -1)
+}
+
 export default class msRawGraphOptimizer {
   msRawGraphOptimizers = [];
 
   constructor() {
     this.msRawGraphOptimizers = [
-      conceptualGraphOptimization,
-      // pruneTupleGetItem,
+      pruneTupleGetItem,
+      keepOnlyBackbone,
     ];
   }
 
