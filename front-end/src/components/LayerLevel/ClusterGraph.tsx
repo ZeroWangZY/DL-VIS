6ec95ab47726f14d97f9132805cbe683eb2344fd
Jingli Xu <1220341948@qq.com>
2020-08-27 15:17:21 +0800

    fix: 聚类图边框

--- front-end/src/components/LayerLevel/ClusterGraph.tsx
+++ front-end/src/components/LayerLevel/ClusterGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
 import TSNE from 'tsne-js';
 import * as d3 from 'd3';
 import { isFunction } from "util";
@@ -16,19 +16,37 @@ interface Props {
 const ClusterGraph: React.FC<Props> = (props: Props) => {
   const { clusterData, clusterStep } = props;
   const svgRef = useRef();
-  const graphWidth = 310;
-  const graphHeight = 310;
 
   const titleAreaHeight = 16 // graphHeight * 0.1;
-  const chartAreaHeight = graphHeight - titleAreaHeight;
+  // const chartAreaHeight = graphHeight - titleAreaHeight;
 
-  const margin = { left: 10, right: 10, top: 5, bottom: 5 }; // 整个cluster与外层之间的margin
-  const clusterWidth = graphWidth - margin.left - margin.right;
-  const clusterHeight = chartAreaHeight - margin.top - margin.bottom;
+  // const margin = { left: 10, right: 10, top: 5, bottom: 5 }; // 整个cluster与外层之间的margin
+  // const clusterWidth = graphWidth - margin.left - margin.right;
+  // const clusterHeight = chartAreaHeight - margin.top - margin.bottom;
   const processedGraph = useProcessedGraph();
   const { nodeMap } = processedGraph;
   const { selectedNodeId } = useGlobalStates();
 
+  // new 
+  const containerHeight = 310;
+  const [containerWidth, setContainerWidth] = useState(260);
+
+  const margin = { left: 40, right: 40, top: 16, bottom: 20 }; // cluster graph画布 外层 的div 更外层div之间的便宜偏移
+  const measuredRef = useCallback((node) => {
+    if (node !== null) {
+      setContainerWidth(node.getBoundingClientRect().width);
+    }
+  }, []);
+  console.log(containerWidth);
+  let clusterGraphContainerHeight = containerHeight - margin.top - margin.bottom - titleAreaHeight;
+  let clusterGraphContainerWidth = containerWidth - margin.left - margin.right;
+
+  console.log("clusterGraphContainerWidth", clusterGraphContainerWidth);
+
+  const margin2 = { left: 4, top: 4 };
+  let clusterGraphHeight = clusterGraphContainerHeight - margin2.top * 2;
+  let clusterGraphWidth = clusterGraphContainerWidth - margin2.left * 2;
+  console.log("clusterGraphWidth", clusterGraphWidth);
 
   useEffect(() => {
     if (!clusterData || clusterData.length === 0 || !clusterStep) return;
@@ -47,11 +65,11 @@ const ClusterGraph: React.FC<Props> = (props: Props) => {
 
     let xScale = d3.scaleLinear()
       .domain([minVal_x, maxVal_x])
-      .range([0, clusterWidth]);
+      .range([0, clusterGraphWidth]);
 
     let yScale = d3.scaleLinear()
       .domain([minVal_y, maxVal_y])
-      .range([clusterHeight, 0]);
+      .range([clusterGraphHeight, 0]);
 
     //绘制圆
     let circle = svg.selectAll("g")
@@ -59,10 +77,10 @@ const ClusterGraph: React.FC<Props> = (props: Props) => {
       .enter()
       .append("g")
       .attr('transform', (d) => {
-        let left = margin.left + xScale(d[0]);
-        let top = yScale(d[1]) + margin.top;
-        if (left >= chartAreaHeight - 2) {
-          left = chartAreaHeight - 2;
+        let left = margin2.left + xScale(d[0]);
+        let top = yScale(d[1]) + margin2.top;
+        if (left >= clusterGraphWidth - 2) {
+          left = clusterGraphWidth - 2;
         }
         return `translate(${left}, ${top})`;
       })
@@ -86,12 +104,12 @@ const ClusterGraph: React.FC<Props> = (props: Props) => {
           let y = 0;
           // 判断text的位置是否超出svg的边界
           // x边界的处理
-          if (margin.left + xScale(d[0]) + Math.ceil(width) + 10 >= chartAreaHeight) {
+          if (margin2.left + xScale(d[0]) + Math.ceil(width) + 10 >= clusterGraphWidth) {
             x = -(Math.ceil(width) + 10);
           }
           // y边界的处理
-          if (yScale(d[1]) + margin.top <= Math.ceil(height)) {
-            y = (Math.ceil(height) + 5 - yScale(d[1]) - margin.top);
+          if (yScale(d[1]) + margin2.top <= Math.ceil(height)) {
+            y = (Math.ceil(height) + 5 - yScale(d[1]) - margin2.top);
           }
           text.attr("x", x).attr("y", y);
         }
@@ -102,18 +120,16 @@ const ClusterGraph: React.FC<Props> = (props: Props) => {
         g.select('text').transition().duration(500).style('visibility', 'hidden');
       })
 
-  }, [clusterData, clusterStep])
+  }, [clusterData, clusterStep, containerWidth])
 
   return (
-    <div className="layerLevel-cluster-container" style={{ height: graphHeight }}>
+    <div className="layerLevel-cluster-container" ref={measuredRef} style={{ height: containerHeight }}>
 
       <div
         className="layerLevel-detailInfo-title"
         style={{
           height: titleAreaHeight + "px",
-          width: "95%",
-          position: 'relative',
-          left: margin.left,
+          width: "100%",
           fontSize: "14px"
         }}>
         <span>
@@ -122,10 +138,20 @@ const ClusterGraph: React.FC<Props> = (props: Props) => {
 
       </div>
 
-      <svg
-        style={{ height: chartAreaHeight + "px", width: chartAreaHeight + "px" }}
-        ref={svgRef}>
-      </svg>
+      <div
+        className="layerLevel-cluster-graph"
+        style={{
+          position: "relative",
+          left: margin.left,
+          top: margin.top,
+          height: clusterGraphContainerHeight,
+          width: clusterGraphContainerWidth,
+        }}>
+        <svg
+          style={{ height: clusterGraphContainerHeight + "px", width: clusterGraphContainerWidth + "px" }}
+          ref={svgRef}>
+        </svg>
+      </div>
     </div>
   );
 }
