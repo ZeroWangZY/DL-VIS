6511000a05afe9a0a0e2dd082166ae075317c1bf
Jingli Xu <1220341948@qq.com>
2020-11-05 22:29:01 +0800

    fix: 圆角旅行

--- front-end/src/components/pixijs-render/PixiDraw.jsx
+++ front-end/src/components/pixijs-render/PixiDraw.jsx
@@ -23,46 +23,63 @@ const PixiDraw = () => {
     if (!styledGraph || styledGraph.nodeStyles.length === 0) return;
 
     const app = new PIXI.Application({
-      width: 300,
-      height: 300,
-      antialias: true,
-      transparent: false,
-      resolution: 1,
-      backgroundColor: 0x1d9ce0
+      width: 256,         // default: 800
+      height: 256,        // default: 600
+      antialias: true,    // default: false
+      transparent: true, // default: false
+      resolution: 1       // default: 1
     })
     container.current.appendChild(app.view);
 
-    const circle = new PIXI.Graphics();
-    circle.beginFill(0xfb6a8f);
-    circle.drawCircle(0, 0, 32);
-    circle.endFill();
-    circle.x = 130;
-    circle.y = 130;
+    app.renderer.view.style.position = "absolute";
+    app.renderer.view.style.display = "block";
+    app.renderer.autoResize = true;
+    app.renderer.resize(window.innerWidth, window.innerHeight);
 
-    circle.scale.set(1.5, 1);
-    circle.interactive = true;
-    circle.on("click", () => {
-      circle.alpha = 0.5;
-    })
-
-    app.stage.addChild(circle);
-
-    app.ticker.add(() => {
-      // 每秒调用该方法60次(60帧动画)
-      circle.rotation += 0.01;
-    })
-    // addNodes(styledGraph);
+    addNodes(app, styledGraph);
 
   }, [styledGraph]);
 
-  const addNodes = (styledGraph) => {
+  const addNodes = (app, styledGraph) => {
     const nodeStyles = styledGraph.nodeStyles;
+
+    styledGraph.nodeStyles.forEach((d) => {
+      const node = d.data;
+      if (node.type === NodeType.OPERATION) {
+        // 
+
+        if (node.parameters.length !== 0) {
+          // let circle = 
+        }
+
+        if (node.constVals.length !== 0) {
+
+        }
+      } else if (node.type === NodeType.GROUP || node.type === NodeType.DATA) {
+        let roundBox = new PIXI.Graphics();
+        roundBox.lineStyle(1, 0x808080, 1); // width color alpha
+        roundBox.drawRoundedRect(
+          d.style._gNodeTransX - d.style._rectWidth / 2,
+          d.style._gNodeTransY - d.style._rectHeight / 2,
+          d.style._rectWidth,
+          d.style._rectHeight,
+          5);
+        roundBox.endFill();
+
+        app.stage.addChild(roundBox);
+
+      } else if (node.type === NodeType.LAYER) {
+
+      }
+    })
+
+
   }
 
   return (
     <div className="pixi-container"
       ref={container}
-      style={{ width: "100%", height: "100%", textAlign: "left" }}
+      style={{ width: "100%", height: "100%", textAlign: "left", padding: 0, margin: 0 }}
     >
 
     </div>
