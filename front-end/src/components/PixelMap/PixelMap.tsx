ad0ea86ec62aa6715ae2dcb3c78c50a846fdce3f
ZeroWangZY <756762961@qq.com>
2021-01-05 16:52:37 +0800

    feat: 增加热力图的绘制

--- front-end/src/components/PixelMap/PixelMap.tsx
+++ front-end/src/components/PixelMap/PixelMap.tsx
@@ -1,10 +1,56 @@
-import React from "react";
-
+import React, { useRef, useEffect, useState } from "react";
+import * as d3 from "d3";
+import { getTensorHeatmapSequential } from "../../api/layerlevel";
 
 const PixelMap: React.FC = () => {
-  return <div className="pixel-map-container">
-      this is pixel map
-  </div>;
+  const svgRef = useRef();
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    getTensorHeatmapSequential({
+      start_step: 5000,
+      end_step: 5500,
+    }).then((data) => setData(data.data.data));
+  }, []);
+
+  useEffect(() => {
+    if (data === null) return;
+    console.log(d3.max(data.data));
+    const color = d3.scaleSequential(d3.interpolatePlasma);
+    const svgD3 = d3.select(svgRef.current);
+    const x = d3
+      .scaleLinear()
+      .domain([0, data.data.length])
+      .rangeRound([5, 1550]);
+    const y = d3.scaleLinear().domain([0, 19]).rangeRound([5, 295]);
+    const z = d3.scaleLinear().domain([-0.5, 0.5]).rangeRound([0, 1]);
+    const yAxis = (g) =>
+      g
+        .attr("transform", `translate(5,0)`)
+        .call(d3.axisLeft(y).tickSize(0))
+        .call((g) => g.select(".domain").remove());
+    svgD3.attr("font-family", "sans-serif").attr("font-size", 10);
+
+    svgD3
+      .append("g")
+      .selectAll("g")
+      .data(data.data)
+      .enter()
+      .append("g")
+      .attr("transform", (d, i) => `translate(${x(i)},0)`)
+      .selectAll("rect")
+      .data((d: any) => d)
+      .enter()
+      .append("rect")
+      .attr("y", (d, i) => y(i + 1))
+      .attr("height", (d, i) => y(i + 1) - y(i))
+      .attr("width", (d, i) => x(i + 1) - x(i))
+      .attr("fill", (d: any) => color(z(d)));
+  }, [data]);
+  return (
+    <div className="pixel-map-container">
+      <svg style={{ height: 300, width: "100%" }} ref={svgRef}></svg>
+    </div>
+  );
 };
 
 export default PixelMap;
