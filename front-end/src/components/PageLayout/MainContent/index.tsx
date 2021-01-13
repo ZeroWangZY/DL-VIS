332227f184eb9056e7226e19147ffae2bde85951
ZeroWangZY <756762961@qq.com>
2021-01-13 11:05:20 +0800

    删除2

--- front-end/src/components/PageLayout/MainContent/index.tsx
+++ front-end/src/components/PageLayout/MainContent/index.tsx
@@ -1,127 +1,88 @@
 import React, { useState, useEffect } from "react";
 import FlexHorContainer from "../FlexHorContainer";
 import FlexVerContainer from "../FlexVerContainer";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./index.less";
 import ELKLayout from "../../ELKLayout/ELKLayout";
 import PixiDraw from "../../pixijs-render/PixiDraw";
 import NodeInfoCard from "../../NodeInfoCard/NodeInfoCard";
 import Legend from "../../Legend";
-import {
-  useGlobalConfigurations
-} from "../../../store/global-configuration";
+import { useGlobalConfigurations } from "../../../store/global-configuration";
 
 let bottomVisibility = false;
 let rightVisibility = false;
 
 export default () => {
-  const [fixedHeight, setFixedHeight] = useState("360px");
-  const { conceptualGraphMode, webGLMode, pixiJSMode } = useGlobalConfigurations();
+  const { pixiJSMode } = useGlobalConfigurations();
 
   return (
-    <Router>
-      <FlexVerContainer
-        fixedHeight={fixedHeight}
-        stretchItem="top"
-        renderBottomChild={(onHide, onShow, visibility) => {
-          return (
-            <div className="vertical-bottom-wrapper">
-              <div
-                className="expand-btn"
-                onClick={visibility ? onHide : onShow}
-              >
-                {bottomVisibility = visibility}
-                {visibility ? (
-                  <img
-                    src={process.env.PUBLIC_URL + "/assets/down.svg"}
-                    alt="收起"
-                  />
-                ) : (
-                    <img
-                      src={process.env.PUBLIC_URL + "/assets/up.svg"}
-                      alt="展开"
-                    />
-                  )}
-              </div>
-            </div>
-          );
-        }}
-        renderTopChild={() => (
-          <FlexHorContainer
-            stretchItem="left"
-            fixedWidth="200px"
-            renderLeftChild={() => (
-              <Switch>
-                <Route path="/">
-                  {/* {!webGLMode && <ELKLayout />}
+    <FlexHorContainer
+      stretchItem="left"
+      fixedWidth="200px"
+      renderLeftChild={() => (
+        <>
+          {/* {!webGLMode && <ELKLayout />}
                   {webGLMode && <RenderGraph />} */}
-                  {!pixiJSMode && <ELKLayout />}
-                  {pixiJSMode && <PixiDraw
-                    bottomVisibility={bottomVisibility}
-                    rightVisibility={rightVisibility} />}
-                </Route>
-              </Switch>
+          {!pixiJSMode && <ELKLayout />}
+          {pixiJSMode && (
+            <PixiDraw
+              bottomVisibility={bottomVisibility}
+              rightVisibility={rightVisibility}
+            />
+          )}
+        </>
+      )}
+      renderRightChild={(onHide, onShow, visibility) => (
+        <div className="hor-right-wrapper" style={{ height: "100%" }}>
+          <div className="expand-btn" onClick={visibility ? onHide : onShow}>
+            {(rightVisibility = visibility)}
+            {visibility ? (
+              <img
+                src={process.env.PUBLIC_URL + "/assets/right.svg"}
+                alt="收起"
+              />
+            ) : (
+              <img
+                src={process.env.PUBLIC_URL + "/assets/left.svg"}
+                alt="展开"
+              />
             )}
-            renderRightChild={(onHide, onShow, visibility) => (
-              <div className="hor-right-wrapper" style={{ height: "100%" }}>
+          </div>
+          <FlexVerContainer
+            fixedHeight="280px"
+            stretchItem="top"
+            renderTopChild={() => (
+              <div className="info-wrapper">
+                <div className="panel-title">节点属性</div>
+                <NodeInfoCard />
+              </div>
+            )}
+            renderBottomChild={(onHide, onShow, visibility) => (
+              <div className="legend-wrapper" style={{ height: "100%" }}>
+                <Legend />
                 <div
                   className="expand-btn"
                   onClick={visibility ? onHide : onShow}
                 >
-                  {rightVisibility = visibility}
-                  {visibility ? (
-                    <img
-                      src={process.env.PUBLIC_URL + "/assets/right.svg"}
-                      alt="收起"
-                    />
-                  ) : (
+                  <span className="legend-title">图例</span>
+                  <span className="btn">
+                    {visibility ? (
                       <img
-                        src={process.env.PUBLIC_URL + "/assets/left.svg"}
+                        src={process.env.PUBLIC_URL + "/assets/down-b.svg"}
+                        alt="收起"
+                      />
+                    ) : (
+                      <img
+                        src={process.env.PUBLIC_URL + "/assets/up-b.svg"}
                         alt="展开"
                       />
                     )}
+                  </span>
                 </div>
-                <FlexVerContainer
-                  fixedHeight="280px"
-                  stretchItem="top"
-                  renderTopChild={() => (
-                    <div className="info-wrapper">
-                      <div className="panel-title">节点属性</div>
-                      <NodeInfoCard />
-                    </div>
-                  )}
-                  renderBottomChild={(onHide, onShow, visibility) => (
-                    <div className="legend-wrapper" style={{ height: "100%" }}>
-                      <Legend />
-                      <div
-                        className="expand-btn"
-                        onClick={visibility ? onHide : onShow}
-                      >
-                        <span className="legend-title">图例</span>
-                        <span className="btn">
-                          {visibility ? (
-                            <img
-                              src={
-                                process.env.PUBLIC_URL + "/assets/down-b.svg"
-                              }
-                              alt="收起"
-                            />
-                          ) : (
-                              <img
-                                src={process.env.PUBLIC_URL + "/assets/up-b.svg"}
-                                alt="展开"
-                              />
-                            )}
-                        </span>
-                      </div>
-                    </div>
-                  )}
-                />
               </div>
             )}
           />
-        )}
-      />
-    </Router>
+        </div>
+      )}
+    />
   );
 };
