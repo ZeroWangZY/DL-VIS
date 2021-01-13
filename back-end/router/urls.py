88ee94a48083a78f3186e079d167981c28945fc1
Carl Wang <715545827@qq.com>
2021-01-13 13:07:05 +0800

    feat: simplified back-end

--- back-end/router/urls.py
+++ back-end/router/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -21,16 +20,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/ms/get_summary_graph', views.get_summary_graph),
     path('api/get_local_ms_graph', views.get_local_ms_graph),
-
-    path('api/get_model_scalars', views.get_model_scalars),
-    path('api/get_metadata', views.get_metadata),
-    path('api/get_node_scalars', views.get_node_scalars),
-    path('api/get_node_tensors', views.get_node_tensors),
-    path('api/get_node_tensor', views.get_node_tensor),
-    path('api/get_layer_scalars', views.get_layer_scalars),
-    path('api/get_cluster_data', views.get_cluster_data),
-    path('api/get_node_line', views.get_node_line),
-    path('api/get_tensor_heatmap', views.get_tensor_heatmap),
 
     path('api/emit_action', views.emit_action)
 ]
